c6fe09c19fc7a94c6493f4f8f4c675ded124f701
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-13 18:41:46 -0300

    feat: Add filters styles.

---
+++ src/components/FiltersSection/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./FiltersSection.module.css";
+export function FiltersSection() {
+  return (
+    <>
+      <section className={styles.filter_container}>
+        <ul className={styles.filterBox}>
+          <li>
+            <button className={styles.filter}>Todas</button>
+          </li>
+          <li>
+            <button className={styles.filter}>Em Promoção</button>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+}
