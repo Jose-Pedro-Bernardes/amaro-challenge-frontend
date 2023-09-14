69d0318be7ee0092df03ac6611ada23e150e8e47
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-13 23:50:45 -0300

    feat: interface to products json.

---
+++ src/types/products.interface.ts
@@ -0,0 +1,22 @@
+export interface IProduct {
+  name: string;
+  style: string;
+  code_color: string;
+  color_slug: string;
+  color: string;
+  on_sale: boolean;
+  regular_price: string;
+  actual_price: string;
+  discount_percentage: string;
+  installments: string;
+  image: string;
+  sizes: {
+    available: boolean;
+    size: string;
+    sku: string;
+  }[];
+}
+
+export interface IProductList {
+  products: IProduct[];
+}
