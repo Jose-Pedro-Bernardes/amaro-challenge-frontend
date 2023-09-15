ee3ae3a7ff199da5cad4f53619b4ae9336bd6b05
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-14 22:14:11 -0300

    feat: add new component, card. Define all styles.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,116 +1,44 @@
 import { IProduct } from "@/types/products.interface";
-import React, { useState } from "react";
-import Image from "next/image";
 import styles from "./Card.module.css";
+import Image from "next/image";
+import { capitalizeWords } from "@/helpers/capitalizeWords";
 
 interface IProps {
   key: string;
-  product: IProduct;
+  clothes: IProduct;
 }
 
-const first = {
-  name: "VESTIDO TRANSPASSE BOW",
-  style: "20002605",
-  code_color: "20002605_613",
-  color_slug: "tapecaria",
-  color: "TAPEÇARIA",
-  on_sale: false,
-  regular_price: "R$ 199,90",
-  actual_price: "R$ 199,90",
-  discount_percentage: "",
-  installments: "3x R$ 66,63",
-  image:
-    "https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912",
-  sizes: [
-    {
-      available: false,
-      size: "PP",
-      sku: "5807_343_0_PP",
-    },
-    {
-      available: true,
-      size: "P",
-      sku: "5807_343_0_P",
-    },
-    {
-      available: true,
-      size: "M",
-      sku: "5807_343_0_M",
-    },
-    {
-      available: true,
-      size: "G",
-      sku: "5807_343_0_G",
-    },
-    {
-      available: false,
-      size: "GG",
-      sku: "5807_343_0_GG",
-    },
-  ],
-};
-
-const second = {
-  name: "VESTIDO FRANZIDO RECORTES",
-  style: "20001609",
-  code_color: "20001609_029",
-  color_slug: "preto",
-  color: "PRETO",
-  on_sale: true,
-  regular_price: "R$ 139,90",
-  actual_price: "R$ 69,90",
-  discount_percentage: "50%",
-  installments: "2x R$ 34,95",
-  image:
-    "https://d3l7rqep7l31az.cloudfront.net/images/products/20001609_002_catalog_1.jpg?",
-  sizes: [
-    {
-      available: true,
-      size: "PP",
-      sku: "3627_40130843_0_PP",
-    },
-    {
-      available: true,
-      size: "P",
-      sku: "3627_40130843_0_P",
-    },
-    {
-      available: true,
-      size: "M",
-      sku: "3627_40130843_0_M",
-    },
-    {
-      available: false,
-      size: "G",
-      sku: "3627_40130843_0_G",
-    },
-    {
-      available: false,
-      size: "GG",
-      sku: "3627_40130843_0_GG",
-    },
-  ],
-};
-
-export default function Card() {
-  const [sale, setSale] = useState(false);
-
-  function onSaleOrRegular(regularPrice: string, actualPrice: string) {
-    if (regularPrice !== actualPrice) {
-      setSale(true);
-    }
-  }
+export default function Card({ key, clothes }: IProps) {
   return (
     <>
-      <li className={styles.card}>
+      <li key={clothes.name} className={styles.card}>
+        {clothes.on_sale && (
+          <div className={styles.percentage_box}>
+            <p>-{clothes.discount_percentage}</p>
+          </div>
+        )}
         <figure>
-          <Image src={second.image} alt={first.name} width={450} height={250} />
+          <Image
+            src={clothes.image}
+            alt={clothes.name}
+            width={450}
+            height={250}
+            layout="responsive"
+          />
         </figure>
         <div>
-          <div>
-            <h2>{first.name}</h2>
-            <p>{first.color_slug}</p>
-            <h3>{first.actual_price}</h3>
+          <div className={styles.card_text__align}>
+            <h2 className={styles.product_name}>
+              {capitalizeWords(clothes.name)}
+            </h2>
+            <div className={styles.price_line}>
+              <h3 className={styles.price}>{clothes.actual_price}</h3>
+              {clothes.on_sale && (
+                <p className={styles.promo}>{clothes.regular_price}</p>
+              )}
+            </div>
+            <p className={styles.installments}>{clothes.installments}</p>
+            <button className={styles.addToCart}>Adicionar ao Carrinho</button>
           </div>
         </div>
       </li>
