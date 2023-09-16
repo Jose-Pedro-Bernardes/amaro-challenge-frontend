85bdc2a1ec1a1bb016ac431cc4b6bd09e8bc15b0
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-15 21:45:01 -0300

    feat: passing input values ​​to the filtering function and adding cartCount to the cart.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
+import { IHeaderProps } from "./header.interface";
 import styles from "./Header.module.css";
+import Image from "next/image";
 import Link from "next/link";
-import { ChangeEvent, MouseEventHandler } from "react";
-import { useState } from "react";
 
-interface HeaderProps {
-  handleSearch: (e: ChangeEvent<HTMLInputElement>) => void;
-  handleSubmit: MouseEventHandler<HTMLButtonElement>;
-  inputValue: string;
-}
-
-export function Header() {
+export function Header({ search, setSearch, cartCount }: IHeaderProps) {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.header__container_align}>
@@ -31,23 +24,18 @@ export function Header() {
               width={22}
               height={30}
             />
-            <p className={styles.cart_count}>1</p>
+            <p className={styles.cart_count}>{cartCount}</p>
           </Link>
         </div>
         <form className={styles.boxInputSearch}>
           <input
-            //value={inputValue}
+            value={search}
             className={styles.searchInput}
-            //onChange={handleSearch}
-            maxLength={40}
+            onChange={(e) => setSearch(e.target.value)}
             type="text"
             placeholder="O que você está procurando?"
           />
-          <button
-            type={"button"}
-            //onClick={handleSubmit}
-            className={styles.buttonSearch}
-          >
+          <button type={"button"} className={styles.buttonSearch}>
             {" "}
             <Image
               src={"/images/lupa.svg"}
