bee3fa38bbd16088c904645e143ed2113a311b61
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-15 21:46:40 -0300

    feat: promotion filters and all working.

--- src/components/FiltersSection/index.tsx
+++ src/components/FiltersSection/index.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import styles from "./FiltersSection.module.css";
-export function FiltersSection({}) {
+import { IProduct } from "@/types/products.interface";
+import { IPropsFilters } from "./filterSection.interface";
+
+export function FiltersSection({ clothes, setFilteredClothes }: IPropsFilters) {
+  function filteredSaleProduct(clothes: IProduct[]) {
+    if (!clothes) {
+      setFilteredClothes([]);
+    }
+    const filter = clothes.filter((product) => product.on_sale === true);
+    setFilteredClothes(filter);
+  }
   return (
     <>
       <section className={styles.filter_container}>
         <ul className={styles.filterBox}>
           <li>
-            <button className={styles.filter}>Todas</button>
+            <button
+              onClick={() => filteredSaleProduct([])}
+              className={styles.filter}
+            >
+              Todos
+            </button>
           </li>
           <li>
-            <button className={styles.filter}>Em Promoção</button>
+            <button
+              onClick={() => filteredSaleProduct(clothes)}
+              className={styles.filter}
+            >
+              Em Promoção
+            </button>
           </li>
         </ul>
       </section>
