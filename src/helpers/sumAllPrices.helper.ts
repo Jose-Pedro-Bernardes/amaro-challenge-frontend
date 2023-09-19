1b68cf50bc3af6d3117da5e9bbd4365d54375ef4
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-19 20:51:42 -0300

    feat: add helper Function formatPrice in other function sumAllPrices.

--- src/helpers/sumAllPrices.helper.ts
+++ src/helpers/sumAllPrices.helper.ts
@@ -1,11 +1,12 @@
 import { IProductCart } from "@/types/products.interface";
 import removeCurrencyFormatting from "./removeCurrencyFormatting.helper";
+import formatPrice from "./formatPrice.helper";
 
-export default function totalValue(cart: IProductCart[]): number {
+export default function sumAllPrices(cart: IProductCart[]): string {
   const sum = cart.reduce((prevValue: number, currentValue: IProductCart) => {
     const priceNumber = removeCurrencyFormatting(currentValue.actual_price);
     return prevValue + priceNumber;
   }, 0);
 
-  return sum;
+  return formatPrice(sum);
 }
