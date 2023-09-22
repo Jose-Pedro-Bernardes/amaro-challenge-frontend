7ab2bc1393db423f5676913eac34e63a4e14bf5f
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-21 21:56:18 -0300

    feat: add condition in function sumAllPrices, a boolean to prices.

--- src/helpers/sumAllPrices.helper.ts
+++ src/helpers/sumAllPrices.helper.ts
@@ -2,7 +2,18 @@ import { IProductCart } from "@/types/products.interface";
 import removeCurrencyFormatting from "./removeCurrencyFormatting.helper";
 import formatPrice from "./formatPrice.helper";
 
-export default function sumAllPrices(cart: IProductCart[]): string {
+export default function sumAllPrices(
+  cart: IProductCart[],
+  regular: boolean
+): string {
+  if (regular) {
+    const sum = cart.reduce((prevValue: number, currentValue: IProductCart) => {
+      const priceNumber = removeCurrencyFormatting(currentValue.regular_price);
+      return prevValue + priceNumber;
+    }, 0);
+
+    return formatPrice(sum);
+  }
   const sum = cart.reduce((prevValue: number, currentValue: IProductCart) => {
     const priceNumber = removeCurrencyFormatting(currentValue.actual_price);
     return prevValue + priceNumber;
