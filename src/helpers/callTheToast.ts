e841bc6ac6954eccb886271b035629bdd863a386
jose-pedro <josepedro.bernardes1@gmail.com>
2023-09-25 15:09:32 -0300

    feat: add new function to call errorToast

--- src/helpers/callTheToast.ts
+++ src/helpers/callTheToast.ts
@@ -12,3 +12,16 @@ export function callTheToast() {
     theme: "light",
   });
 }
+
+export function callTheErrorToast() {
+  toast.error("Escolha um tamanho.", {
+    position: "top-center",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+}
